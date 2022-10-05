ee60b3f2714e338061d4a3be8508e770d146808d
unknown <jpz4373@linecorp.com>
2022-10-05 14:22:51 +0900

    listeners

---
+++ src/main/java/ExtentReporter/ExtendReporterNG.java
@@ -0,0 +1,22 @@
+package ExtentReporter;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+public class ExtendReporterNG {
+	public static ExtentReports getReportObject()
+	{
+		String path = System.getProperty("user.dir"+"//reports//index.html");
+		ExtentSparkReporter reporter = new ExtentSparkReporter(path);
+		reporter.config().setReportName("Selenium training");
+		reporter.config().setDocumentTitle("Test Results");
+		ExtentReports extent = new ExtentReports();
+		extent.attachReporter(reporter);
+		extent.setSystemInfo("Tester", "Josh");
+		extent.createTest(path);
+		return extent;
+		
+		
+	}
+
+}
---
+++ src/test/java/frameworkPractice/testComponents/Listeners.java
@@ -0,0 +1,86 @@
+package frameworkPractice.testComponents;
+
+import java.io.IOException;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+
+import ExtentReporter.ExtendReporterNG;
+
+public class Listeners extends BaseTest implements ITestListener {
+	ExtentTest test;
+	 ExtentReports extent = ExtendReporterNG.getReportObject();
+	@Override
+	public void onTestStart(ITestResult result) {
+		// TODO Auto-generated method stub
+		test =extent.createTest(result.getMethod().getMethodName());
+	}
+
+	@Override
+	public void onTestSuccess(ITestResult result) {
+		// TODO Auto-generated method stub
+		test.log(Status.PASS, "成功");
+	}
+
+	@Override
+	public void onTestFailure(ITestResult result) {
+		// TODO Auto-generated method stub
+		test.log(Status.FAIL, "失敗");
+		test.fail(result.getThrowable());
+		try {
+			driver= (WebDriver) result.getTestClass().getRealClass().getField("driver")
+					.get(result.getInstance());
+		} catch (Exception e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} 
+		
+		String filePath = null;
+		try {
+			filePath = getScreenshot(result.getMethod().getMethodName(),driver);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		test.addScreenCaptureFromPath(filePath,result.getMethod().getMethodName() );
+
+
+	}
+
+	@Override
+	public void onTestSkipped(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTestFailedWithTimeout(ITestResult result) {
+		// TODO Auto-generated method stub
+		;
+	}
+
+	@Override
+	public void onStart(ITestContext context) {
+		// TODO Auto-generated method stub
+		;
+	}
+
+	@Override
+	public void onFinish(ITestContext context) {
+		// TODO Auto-generated method stub
+		extent.flush();
+	}
+	
+}
